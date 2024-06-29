bda37a76a60820a0b35dd9e82182720734518997
Gokulcasper <ronitcasper215@gmail.com>
2024-06-29 06:26:38 +0530

    Add Element to Array

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+// const value=["string",1,true,undefined,null];
+
+// Adding Element to an array
+let cart=["apple","banana","lemon"];
+// push 
+cart.push("orange");
+// unshift
+cart.unshift("Grapes","watermelons");
+// splice
+cart.splice(1,1,"starfruit");
+
+console.log(cart)
